0e22d2ed4282a5b6b0b5cbd269fea3463e9780f5
Giannina Binder <giannina@haldenweg.ch>
2022-05-24 12:02:19 +0200

    did the controller

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,2 +1,28 @@
 class EquipmentsController < ApplicationController
+  def index
+    @equipments = Equipment.all
+  end
+
+  def show
+    @equipment = Equipment.find(params[:id])
+  end
+
+  def new
+    @equipment = Equipment.new
+  end
+
+  def create
+    @equipment = Equipment.new(equipment_params)
+    if @equipment.save
+      redirect_to equipment_path(@equipment)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def equipment_params
+    params.require(:equipment).permit(:user_id, :name, :type, :description, :price)
+  end
 end
