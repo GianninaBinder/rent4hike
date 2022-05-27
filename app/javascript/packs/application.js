2c8b2681d0d09540c689d7f45f816cd9686622f5
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-27 14:15:05 +0200

    added select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,17 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+import $ from 'jquery'
+import 'select2/dist/css/select2.css'
+import 'select2'
+
+/// if you don't have turnbolinks then use this:
+//// document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('turbolinks:load', () => {
+  $('.select2').select2({
+    tags: true,
+    tokenSeparators: [',', ' '],
+    multiple: false
+  });
+})
